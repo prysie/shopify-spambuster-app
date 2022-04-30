a3c9fd3a2089a982161f39a34c6ad71ec53f6f06
Matt Pryse <prysie@gmail.com>
2022-04-30 15:31:10 +1000

    More links

--- src/reducers/root.js
+++ src/reducers/root.js
@@ -39,7 +39,7 @@ export const getInitialState = () => {
     contactEnabled: false,
 
     errorMessageContact: '',
-    enablementLink: 'http://www.google.com',
+    enablementLink: 'https://' + originShop + '/admin/themes/current/editor?context=apps' + '&template=' + '$' +'&activateAppId=' + '7f2f526f-ddcb-445c-9bc7-75d61af80dcb' + '/' + 'reCAPTCHA-spambuster',
     showContactUpdateSuccess: false
   })
 }
@@ -58,7 +58,7 @@ const rootReducer = (state, action) => {
     case APPSTATUS_GET_DONE:
       state = state.set('hasScriptTag', action.payload.hasScriptTag)
       state = state.set('contactEnabled', action.payload.contactEnabled === true)
-      state = state.set('enablementLink', 'https://' + shop + '/admin/themes/current/editor?context=apps' + '&template=' + '$' +'&activateAppId=' + '7f2f526f-ddcb-445c-9bc7-75d61af80dcb' + '/' + 'reCAPTCHA-spambuster')
+      state = state.set('enablementLink', 'https://' + originShop + '/admin/themes/current/editor?context=apps' + '&template=' + '$' +'&activateAppId=' + '7f2f526f-ddcb-445c-9bc7-75d61af80dcb' + '/' + 'reCAPTCHA-spambuster')
       state = state.set('isLoading', false)
       return state
     case INSTALL_GET_DONE:
