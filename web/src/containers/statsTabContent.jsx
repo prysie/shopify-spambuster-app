fcc86de4e79118f88535a0d7e04886587af80631
prysie <prysie@gmail.com>
2024-06-16 17:13:05 +1000

    Update Enterpruse version

--- web/src/containers/statsTabContent.jsx
+++ web/src/containers/statsTabContent.jsx
@@ -1,51 +1,66 @@
 import React from 'react';
 import { Card, FormLayout, DataTable, DatePicker } from '@shopify/polaris';
 
-const StatsTabContent = ({ startDate, endDate, setStartDate, setEndDate, recaptchaActivity }) => {
-  const handleStartDateChange = (value) => {
-    setStartDate(value);
-  };
+class StatsTabContent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        startDate: new Date(), // Set startDate to current date
+        endDate: new Date(), // Set endDate to current date
+      }    
+    this.handleStartDateChange = this.handleStartDateChange.bind(this);
+    this.handleEndDateChange = this.handleEndDateChange.bind(this);
+  }
 
-  const handleEndDateChange = (value) => {
-    setEndDate(value);
-  };
+  handleStartDateChange(value) {
+    this.props.setStartDate(value);
+  }
 
-  return (
-    <>
-      <Card sectioned>
-        <FormLayout>
-          <FormLayout.Group>
-            <DatePicker
-              month={new Date().getMonth()}
-              year={new Date().getFullYear()}
-              onChange={handleStartDateChange}
-              selected={startDate}
-            />
-            <DatePicker
-              month={new Date().getMonth()}
-              year={new Date().getFullYear()}
-              onChange={handleEndDateChange}
-              selected={endDate}
-            />
-          </FormLayout.Group>
-        </FormLayout>
-      </Card>
-      <Card title="reCAPTCHA Activity Blotter" sectioned>
-        <DataTable
+  handleEndDateChange(value) {
+    this.props.setEndDate(value);
+  }
+
+  render() {
+    const { startDate, endDate, recaptchaActivity } = this.props;
+
+    return (
+      <>
+        <Card sectioned>
+          <FormLayout>
+            <FormLayout.Group>
+              <DatePicker
+                month={new Date().getMonth()}
+                year={new Date().getFullYear()}
+                onChange={this.handleStartDateChange}
+                selected={startDate}
+              />
+              <DatePicker
+                month={new Date().getMonth()}
+                year={new Date().getFullYear()}
+                onChange={this.handleEndDateChange}
+                selected={endDate}
+              />
+            </FormLayout.Group>
+          </FormLayout>
+        </Card>
+        <Card title="reCAPTCHA Activity Blotter" sectioned>
+          <DataTable
             columnContentTypes={['text', 'text', 'text', 'numeric']}
             headings={['Timestamp', 'Action', 'Result', 'Score']}
             rows={recaptchaActivity.map(activity => [
-                activity.timestamp,
-                activity.action,
-                activity.result,
-                activity.score,
+              activity.timestamp,
+              activity.action,
+              activity.result,
+              activity.score,
             ])}
-        />
-      </Card>
-      <Card title="reCAPTCHA Activity Graph" sectioned>
-        {/* Render the graph component here */}
-      </Card>
-    </>
-  );
-};
-export default StatsTabContent
+          />
+        </Card>
+        <Card title="reCAPTCHA Activity Graph" sectioned>
+          {/* Render the graph component here */}
+        </Card>
+      </>
+    );
+  }
+}
+
+export default StatsTabContent;
