35c36c4e262e100217c2b4f88f5a06f3af02a3d2
prysie <prysie@gmail.com>
2024-06-21 21:22:54 +1000

    Updated

--- web/src/config.js
+++ web/src/config.js
@@ -1,4 +1,4 @@
 const _BACKEND_URL = process.env.NODE_ENV === 'production' ? 'https://qorqmyn3zb.execute-api.eu-west-1.amazonaws.com/prod' : 'https://ewwntzz1i2.execute-api.eu-west-1.amazonaws.com/dev'
 export const BACKEND_URL = _BACKEND_URL
-export const APP_PATH = process.env.NODE_ENV === 'production' ? '/admin/apps/recaptcha-spambuster' : '/admin/apps/recaptcha-spambuster-dev-1'
+export const APP_PATH = process.env.NODE_ENV === 'production' ? '/admin/apps/recaptcha-spambuster' : '/admin/apps/recaptcha-spambuster-dev'
 export const STAGE = process.env.NODE_ENV === 'production' ? 'prod' : 'dev'
--- web/src/index.js
+++ web/src/index.js
@@ -10,7 +10,7 @@ import { BACKEND_URL, STAGE, APP_PATH } from './config.js'
 import store from './store.js'
 import { get, post } from './utilities.js'
 
-console.log('Spambuster app v2.1.5 - ' + STAGE)
+console.log('Spambuster app v2.1.6 - ' + STAGE)
 
 const render = (apiKey, shop) => {
   console.log('Calling render')
