5fba5e20430e12d535c27540d9878fc38163b763
prysie <prysie@gmail.com>
2024-06-23 09:25:40 +1000

    Updated

--- web/src/index.js
+++ web/src/index.js
@@ -9,7 +9,7 @@ import { BACKEND_URL, STAGE, APP_PATH } from './config.js'
 import store from './store.js'
 import { get, post } from './utilities.js'
 
-console.log('Spambuster app v2.1.20 - ' + STAGE)
+console.log('Spambuster app v2.1.21 - ' + STAGE)
 
 const render = (apiKey, shop) => {
   console.log('Calling render')
